af10517a0a3fcb576ef88a99d814445e3989bfd9
Redone0001 <35457642+Redone0001@users.noreply.github.com>
2018-12-14 16:32:34 +0100

    Add files via upload

--- include/save.h
+++ include/save.h
@@ -2,17 +2,7 @@
 #include <fstream>
 #include <string>
 
-bool saveToCSV(std::string fileName, ordinateurDeBord ENIAC){
+void saveToCSV(std::string fileName, ordinateurDeBord ENIAC){
 	std::ofstream fs (fileName, std::fstream::app);
-	if (! fs.is_open()) return 0;
-	/*
-	pair <float,float> gravite;
-	pair <float,float> position; // la possition de la fussée est repéré dans un référentiel placé au centre de la terre et immobile
-	pair <float,float> vitesse; // la vitesse est dans le même repère que la possition 
-	pair <float,float> acceleration; //idem
-	pair <float,float> SommeForces;
-	float angle; // angle de la fusée / à la verticale
-	*/
-	fs <<ENIAC.position.first<<","<<ENIAC.position.second<<","<<ENIAC.vitesse.first<<","<<ENIAC.vitesse.second<<","<<ENIAC.acceleration.first<<","<<ENIAC.acceleration.second;
-	
+	fs <<ENIAC.position.first<<","<<ENIAC.position.second<<","<<ENIAC.vitesse.first<<","<<ENIAC.vitesse.second<<","<<ENIAC.acceleration.first<<","<<ENIAC.acceleration.second<<"\n";
 }
