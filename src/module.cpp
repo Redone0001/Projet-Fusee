ff9537c6956aae1aef0d09a6eb2dbcf0df6c4131
Redone0001 <35457642+Redone0001@users.noreply.github.com>
2018-12-14 18:02:20 +0100

    Add files via upload

--- src/module.cpp
+++ src/module.cpp
@@ -1,13 +1,8 @@
 #include "../include/module.h"
 using namespace std;
 
-module::module(long double Masse,long double Carburant, long double coef, long double SurfaceFrot):masse(Masse),carburant(Carburant),coefPuissance(coef),surfaceFrot(SurfaceFrot){
-    consomation = 0;
-}
+module::module(long double Masse,long double Carburant, long double power, long double SurfaceFrot, long double conso): masse(Masse), carburant(Carburant), puissance(power), surfaceFrot(SurfaceFrot), consomation(conso){}
 
 module::~module(){}
 
-long double module::puissance(){
-    long double puissance = coefPuissance * consomation;
-    return puissance;
-}
+
